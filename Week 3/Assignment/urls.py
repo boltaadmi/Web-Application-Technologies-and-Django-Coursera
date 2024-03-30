c595d3062d81b8a3f352c9015f31ad18a29f3d62
Amann <91307961+boltaadmi@users.noreply.github.com>
2024-03-30 22:02:22 +0530

    Update urls.py

--- Week 3/Assignment/urls.py
+++ Week 3/Assignment/urls.py
@@ -1,22 +1,15 @@
-"""week3 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from week3 import views
-urlpatterns = [
-    path('week3/', views.week3_assignment),
-    path('admin/', admin.site.urls),
-]
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Jane Instructor 4c56ff ... 454cba ...
+</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+<h1>This is just a starting point</h1>
+<p>Construct your HTML following the instructions below.</p>
+  ....
+<p>When you have finished your HTML *and* it passes the HTML
+ validator, you can submit it to the autograder.</p>
+</body>
+</html>
